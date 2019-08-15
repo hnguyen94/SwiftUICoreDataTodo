a58cafd76a254dc278a27753ea7dd1bc22f23a02
Hoang Nguyen <hoang.nguyen@xing.com>
2019-08-15 08:56:51 +0200

    Initial commit

---
+++ SwiftUICoreDataToDo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUICoreDataToDo
+//
+//  Created by Hoang Nguyen on 15.08.19.
+//  Copyright © 2019 Hoang Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
