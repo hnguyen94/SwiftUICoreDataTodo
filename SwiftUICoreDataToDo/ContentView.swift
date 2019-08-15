459033926a5456196ceb6bb11bbd0716d2955719
Hoang Nguyen <hoang.nguyen@xing.com>
2019-08-15 15:00:04 +0200

    Core data is working

--- SwiftUICoreDataToDo/ContentView.swift
+++ SwiftUICoreDataToDo/ContentView.swift
@@ -22,7 +22,17 @@ struct ContentView: View {
                     HStack {
                         TextField("New Item", text: $newToDoItem)
                         Button(action: {
+                            let toDoItem = ToDoItem(context: self.managedObjectContext)
+                            toDoItem.title = self.newToDoItem
+                            toDoItem.createdAt = Date()
 
+                            do {
+                                try self.managedObjectContext.save()
+                            } catch {
+                                print(error)
+                            }
+
+                            self.newToDoItem = ""
                         }) {
                             Image(systemName: "plus.circle.fill")
                                 .foregroundColor(.green)
@@ -30,11 +40,24 @@ struct ContentView: View {
                         }
                     }
                 }.font(.headline)
-            }
-        }
-        .navigationBarTitle(Text("My List"))
-        .navigationBarItems(trailing: EditButton())
+                Section(header: Text("To Do's")) {
+                    ForEach(self.toDoItems) { toDoItem in
+                        ToDoItemView(title: toDoItem.title!, createdAt: "\(toDoItem.createdAt!)")
+                    }.onDelete { indexSet in
+                        let deleteItem = self.toDoItems[indexSet.first!]
+                        self.managedObjectContext.delete(deleteItem)
 
+                        do {
+                            try self.managedObjectContext.save()
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("My List"))
+            .navigationBarItems(trailing: EditButton())
+        }
     }
 }
 
